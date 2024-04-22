bf2b3ea9b2a1e13110907958406d20e21ec0d634
Aniket Kadukar <166643244+aniketkadukar01@users.noreply.github.com>
2024-04-22 15:16:27 +0530

    Palindrome Number

---
+++ Palindrome.sh
@@ -0,0 +1,12 @@
+
+
+#!/bin/bash
+read -p "Enter the name " name
+reverse=$( echo $name | rev )
+if [[ $name==$reverse ]]
+then
+echo "name $name is palindrome"
+else 
+echo "name $name is not palindrom"
+fi
+
